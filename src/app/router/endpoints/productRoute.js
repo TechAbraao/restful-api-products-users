e86c041ed0a72c7f643837cbf63425a27a255b61
Abraão Santos <abraao.santos@sec4you.com.br>
2025-04-02 23:22:28 -0300

    feat: adding controllers and refactory routes

--- src/app/router/endpoints/productRoute.js
+++ src/app/router/endpoints/productRoute.js
@@ -1,77 +1,36 @@
 import { Router } from "express"
-import ProductDAO from "../../utils/ProductDAO.js";
+import ProductController from "../../controllers/ProductController.js";
 
 const productRoute = Router();
 
-// Listar TODOS os PRODUTOS
-productRoute.get("/produtos", async (req, res) => {
-    try {
-        const allProducts = await ProductDAO.allProducts();
-        res.status(200).json(allProducts)
-    } catch (e) {
-        res.status(500).json({"mensagem": "Erro ao buscar todos os produtos."})
-    }
-})
+/**
+ * @route GET /produtos
+ * @description Retorna uma lista de todos os produtos cadastrados.
+ */
+productRoute.get("/produtos", ProductController.getProducts)
 
-// Listar PRODUTO a partir do ID
-productRoute.get("/produtos/:id", async (req, res) => {
-    try {
-        const id = parseInt(req.params.id)
-        const productById = await ProductDAO.productById(id)
+/**
+ * @route GET /produtos/:id
+ * @description Retorna um produto a partir do ID fornecido.
+ */
+productRoute.get("/produtos/:id", ProductController.getProductsById)
 
-        if (!productById) {
-            return res.status(404).json({"mensagem": "Produto não encontrado."})
-        }
+/**
+ * @route POST /produtos
+ * @description Cria um novo produto no sistema.
+ */
+productRoute.post("/produtos", ProductController.postProducts)
 
-        res.status(200).json(productById)
-    } catch (e) {
-        res.status(500).json({"mensagem": "Erro ao buscar produto através do id."})
-    }
-})
+/**
+ * @route PUT /produtos/:id
+ * @description Atualiza as informações de um produto existente com base no ID fornecido.
+ */
+productRoute.put("/produtos/:id", ProductController.putProductsById)
 
-// Adiciona novo PRODUTO no Banco de Dados
-productRoute.post("/produtos", async (req, res) => {
-    try {
-        console.log("Corpo da requisição recebido:", req.body);
-        const { nome, preco, descricao, quantidade, categoria } = req.body;
-        
-        if (!nome || !preco || !descricao || !quantidade || !categoria) {
-            return res.status(400).json({"mensagem": "Preencha todos os campos para enviar a requisição."})
-        }
-
-        await ProductDAO.addingProduct(nome, preco, descricao, quantidade, categoria)
-
-        res.status(200).json({"mensagem": "Produto adicionado com sucesso."})
-
-    } catch (e) {
-        console.error(`Erro ao adicionar novo produto: ${e}`)
-    }
-})
-
-// Atualizar dados de um PRODUTO existente
-productRoute.put("/produtos/:id", async (req, res) => {
-    try {
-        const id = req.params.id
-        const { nome, preco, descricao, quantidade, categoria } = req.body;
-
-        await ProductDAO.updateProductById(id, nome, preco, descricao, quantidade, categoria)
-
-        res.status(200).json({"mensagem": `Produto atualizado com sucesso.`})
-
-    } catch (e) {
-        console.error(`Erro ao atualizar produto: ${e}`)
-    }
-})
-
-// Remove PRODUTO do Banco de Dados a partir do ID
-productRoute.delete("/produtos/:id", async (req, res) => {
-    const id = req.params.id;
-
-    if (!id) {
-        return res.status(400).json({"mensagem": "Erro ao excluir produto. Especifique o ID."})
-    }
-    await ProductDAO.deleteProductById(id)
-    res.status(200).json({"mensagem": `Produto de id = ${id} excluído com sucesso.`})
-})
+/**
+ * @route DELETE /produtos/:id
+ * @description Remove um produto do sistema com base no ID fornecido.
+ */
+productRoute.delete("/produtos/:id", ProductController.deleteProductsById)
 
 export default productRoute
--- src/app/router/endpoints/userRoute.js
+++ src/app/router/endpoints/userRoute.js
@@ -1,64 +1,36 @@
 import { Router } from "express"
-import UserDAO from "../../utils/UserDAO.js";
+import UserController from "../../controllers/UserController.js";
 
 const userRoute = Router();
 
-// Listar TODOS os USUÁRIOS
-userRoute.get("/usuarios", async (req, res) => {
+/**
+ * @route GET /usuarios
+ * @description Retorna uma lista de todos os usuários cadastrados.
+ */
+userRoute.get("/usuarios", UserController.getUsers)
 
-    const allUsers = await UserDAO.allUsers();
+/**
+ * @route GET /usuarios/:id
+ * @description Retorna um usuário a partir do ID fornecido.
+ */
+userRoute.get("/usuarios/:id", UserController.getUsersById)
 
-    res.status(200).json(allUsers)
-})
-// Listar USUÁRIO por ID
-userRoute.get("/usuarios/:id", async (req, res) => {
-    const id = req.params.id
+/**
+ * @route POST /usuarios
+ * @description Cria um novo usuário no sistema.
+ */
+userRoute.post("/usuarios", UserController.postUsers)
 
-    if (!id) {
-        return res.status(400).json({"mensagem": "Preencha o ID."})
-    }
+/**
+ * @route PUT /usuarios/:id
+ * @description Atualiza as informações de um usuário existente com base no ID fornecido.
+ */
+userRoute.put("/usuarios/:id", UserController.putUsersById)
 
-    const findUserById = await UserDAO.userById(id)
-
-    res.status(200).json(findUserById)
-
-})
-// ADICIONAR USUÁRIO ao Banco de Dados
-userRoute.post("/usuarios", async (req, res) => {
-    const { nome, email, senha } = req.body
-    if (!nome || !email || !senha) {
-        return res.status(400).json({"mensagem": "Preencha todos os campos."})
-    }
-    await UserDAO.addingUser(nome, email, senha)
-    res.status(200).json({"mensagem": "Usuário adicionado com sucesso."})
-
-})
-// ALTERANDO dados de USUÁRIOS
-userRoute.put("/usuarios/:id", async (req, res) => {
-    const id = req.params.id;
-    const { nome, email, senha } = req.body;
-
-    if (!nome || !email || !senha ) {
-        return res.status(400).json({"mensagem": "Preencha todos os campos."})
-    }
-    await UserDAO.updateUserById(id, nome, email, senha);
-
-    res.status(200).json({"mensagem": "Usuário atualizado com sucesso."})
-})
-// DELETANDO um USUÁRIO pelo ID
-userRoute.delete("/usuarios/:id", async (req, res) => {
-    try {
-        const id = req.params.id
-        if (!id) {
-            return res.status(400).json({"mensagem": "Preencha todos os campos."})
-        };
-
-        await UserDAO.deleteUserById(id);
-        res.status(200).json({"mensagem": `Usuário de ID = ${id} excluído com sucesso.`})
-    } catch (e) {
-        console.error(`Erro ao excluir usuário: ${e}`)
-    }
-})
-
+/**
+ * @route DELETE /usuarios/:id
+ * @description Remove um usuário do sistema com base no ID fornecido.
+ */
+userRoute.delete("/usuarios/:id", UserController.deleteUsersById)
 
 export default userRoute
